2fd8ae1a115e5a393f2827f644d62c6d4bd091c0
Alain Herve <alainQtec@gmail.com>
2025-01-17 16:28:10 +0200

    Create New-PythonProject.ps1

---
+++ Public/New-PythonProject.ps1
@@ -0,0 +1,27 @@
+﻿function New-PythonProject {
+  [CmdletBinding(supportsShouldProcess = $true)]
+  param (
+    [Parameter(Mandatory = $false)]
+    [string]$Path = (Resolve-Path .).Path,
+
+    [Parameter(Mandatory = $false)]
+    [Alias('Version')][ValidateScript({
+        if (($_ -as 'version') -is [version] -and $_ -in [pipEnv]::data.PythonVersions) {
+          return $true
+        } else {
+          throw [System.IO.InvalidDataException]::New('Please Provide a valid version')
+        }
+      })]
+    [string]$pythonVersion
+  )
+
+  begin {
+    $Name = Split-Path $Path -Leaf
+  }
+
+  process {
+    if ($PSCmdlet.ShouldProcess(" Create python project", $Name)) {
+      pipenv --python $pythonVersion
+    }
+  }
+}
