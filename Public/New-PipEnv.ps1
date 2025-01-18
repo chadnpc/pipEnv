143ee70f09f8aaaf0f8b7b8cf268d09565079fd1
Alain Herve <alainQtec@gmail.com>
2025-01-18 11:44:36 +0200

    Update New-PipEnv.ps1

--- Public/New-PipEnv.ps1
+++ Public/New-PipEnv.ps1
@@ -1,16 +1,28 @@
 ﻿function New-pipEnv {
+  # .DESCRIPTION
+  #   create a new virtual environment
+  # .LINK
+  #   https://github.com/alainQtec/pipEnv/blob/main/Public/New-PipEnv.ps1
+  # .EXAMPLE
+  #   New-pipEnv
   [CmdletBinding(supportsShouldProcess = $true)]
   [OutputType([Venv])]
   param (
     [Parameter(Mandatory = $false, Position = 0)]
     [ValidateNotNullOrWhiteSpace()]
-    [string]$Name = [pipEnv]::data.venv.Config.defaultName
+    [string]$Path = '.'
   )
-
+  begin {
+    $Path = (Resolve-Path $Path -ea Ignore).Path
+    $Path = [IO.Directory]::Exists($Path) ? $Path : $(throw [System.IO.DirectoryNotFoundException]::new("Directory not found: $Path"))
+    $Name = Split-Path $Path -Leaf; $v = $null
+  }
   process {
-    $v = $null
-    if ($PSCmdlet.ShouldProcess("Create virtual environment")) {
-      $v = [Venv]::Create($Name)
+    if ($PSCmdlet.ShouldProcess("Create virtual environment for $Name", $Path)) {
+      Push-Location $Path
+      [void][venv]::Run("install", "check")
+      Pop-Location
+      $v = [Venv]::Create($Path)
     }
   }
 
