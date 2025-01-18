6b2ccb4d3afba271dba054408e10c841f1ea8d69
Alain Herve <alainQtec@gmail.com>
2025-01-18 20:59:41 +0200

    Create Get-ActivationScript.ps1

---
+++ Public/Get-ActivationScript.ps1
@@ -0,0 +1,11 @@
+﻿function Get-ActivationScript {
+  [CmdletBinding()][OutputType([string])]
+  param (
+    [Parameter(Mandatory = $false)]
+    [string]$Path = (Resolve-Path .).Path
+  )
+
+  process {
+    return [venv]::GetActivationScript($Path)
+  }
+}
