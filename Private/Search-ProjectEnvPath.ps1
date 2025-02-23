fdf2bad30ef5871dce26f99f3d825ea285d4febf
alain <alainQtec@gmail.com>
2025-02-23 21:29:46 +0200

    Create Search-ProjectEnvPath.ps1

---
+++ Private/Search-ProjectEnvPath.ps1
@@ -0,0 +1,28 @@
+﻿function Search-ProjectEnvPath {
+  # .SYNOPSIS
+  #   Search Project's EnvPath
+  # .DESCRIPTION
+  #   Searches in the work home for a project with the same name as the project's directory.
+  # .LINK
+  #   https://github.com/alainQtec/pipEnv/blob/main/Private/Search-ProjectEnvPath.ps1
+  [CmdletBinding()]
+  param (
+    [Parameter(Position = 0, Mandatory = $false, ValueFromPipeline = $true)]
+    [string]$ProjectPath = [IO.Directory]::Exists([venv]::data.ProjectPath) ? [venv]::data.ProjectPath : (Resolve-Path .).Path
+  )
+
+  begin {
+    $reslt = $null; $_env_paths = [venv]::Get_work_Home() | Get-ChildItem -Directory -ea Ignore
+  }
+
+  process {
+    if ($null -ne $_env_paths) {
+      $reslt = $_env_paths.Where({ [IO.File]::ReadAllLines([IO.Path]::Combine($_.FullName, ".project"))[0] -eq $ProjectPath })
+      $reslt = ($reslt.count -eq 0) ? $null : $reslt[0]
+    }
+  }
+
+  end {
+    return $reslt
+  }
+}
